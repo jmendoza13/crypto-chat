2484fea04e3dbd49c4c6e6f510d447491d9fd748
jmendoza13 <justinmendoza13@gmail.com>
2022-07-28 08:48:24 -0700

    removed models/commentjs

---
+++ .history/models/comment_20220728084010.js
@@ -0,0 +1,9 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const commentSchema = new Schema ({
+    
+//     article: [{type: Schema.Types.ObjectId, ref: 'Article'}],
+// });
+
+// module.exports = mongoose.model('Ticket', ticketSchema);
--- models/comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const commentSchema = new Schema ({
-    
-    article: [{type: Schema.Types.ObjectId, ref: 'Article'}],
-});
-
-module.exports = mongoose.model('Ticket', ticketSchema);
